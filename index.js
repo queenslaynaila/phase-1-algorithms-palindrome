9419cc0a84c559d5b871fc8827b06fa25f2e9b8e
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-26 00:30:19 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordReversed = word.split("").reverse().join("");
+  if (word === wordReversed) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  Writet function isPalindrome
+  isPalindrom takes a string as argument
+  it reverses the string letters
+  if the value of the string reversed is the same as the string
+      return false
+      else true
 */
 
 /*
   Add written explanation of your solution here
+  the function takes a variable word reprsenting the string
+  To reverse word 
+      Separate letters in word into an array via split method
+      reverse the order o fthe words via the array reverse methods
+      join the words back together via join 
+  Add an if statement returning true if the string reversed 
+  is the same as its original form .Else return false
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +42,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
